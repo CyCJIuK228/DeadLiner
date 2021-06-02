1aa66e49e2673487cb232760fc0f2e07a8104712
Serhii Yurko <Serhii.Yurko@lnu.edu.ua>
2021-06-02 12:03:56 +0300

    final commit

--- DeadLiner/DeadLinerWebApp.DAL/Migrations/20210512212041_ChangeTaskModel.Designer.cs
+++ DeadLiner/DeadLinerWebApp.DAL/Migrations/20210512212041_ChangeTaskModel.Designer.cs
@@ -4,14 +4,16 @@ using DeadLinerWebApp.DAL.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DeadLinerWebApp.DAL.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210512212041_ChangeTaskModel")]
+    partial class ChangeTaskModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DeadLiner/Testsv2/AccountTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using AutoMapper;
+using DeadLinerWebApp.BLL.Helper;
+using DeadLinerWebApp.BLL.Interfaces;
+using DeadLinerWebApp.BLL.Services;
+using DeadLinerWebApp.DAL.Entity;
+using DeadLinerWebApp.PL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Testsv2;
+
+namespace DeadLinerWebApp.Tests
+{
+    [TestClass]
+    public class AccountTest
+    {
+        private static IAuthorizationService _service;
+
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            MoqArrange.Arrange();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(typeof(MappingProfile)));
+            IMapper mapper = new Mapper(configuration);
+            _service = new AuthorizationService(mapper, MoqArrange.UnitOfWorkMock.Object);
+        }
+
+        [TestMethod]
+        public async void LogIn_NonExistentUser_ReturnNull()
+        {
+            // Arrange 
+            var user = new LoginViewModel
+            {
+                Email = "testEmail",
+                Password = "testPassword"
+            };
+            MoqArrange.UserRepositoryMock.Setup(e => e.Find(It.IsAny<Func<User, bool>>())).Returns(() => null);
+
+            // Act
+            var result = await _service.Login(user, new DefaultHttpContext());
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
